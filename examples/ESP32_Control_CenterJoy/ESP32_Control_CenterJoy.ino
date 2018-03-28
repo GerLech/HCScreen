04f42bdb8f39705ca80f0a8a46e189c152abcf8d
Gerald Lechner <lechge@gmail.com>
2018-03-28 17:54:01 +0200

    new and updated examples

--- examples/ESP32_Control_CenterJoy/ESP32_Control_CenterJoy.ino
+++ examples/ESP32_Control_CenterJoy/ESP32_Control_CenterJoy.ino
@@ -19,21 +19,24 @@
   CD-MISO -> GPIO19 (MISO)
   CD-SCK -> GPIO18 (SCK)
 
-  For input a rotary encoder will be used
+  For input a thumb joystick will be used
 
-  Rotary Encoder -> ESP32
-  SW -> GPIO15
-  DT -> GPIO14
-  CLK -> GPIO4
+  Joy-Stick -> ESP32
+  SW -> GPIO4
+  VRy -> GPIO14
+  VRx -> GPIO15
 
   Written by Gerald Lechner
   MIT license, all text above must be included in any redistribution
  ****************************************************/
 
+
+
+
 #include <Adafruit_GFX.h>    // Core graphics library
 #include <Adafruit_ST7735.h> // Hardware-specific library
+
 #include <HCScreen.h>
-#include <RotaryEncoder.h>
 
 #include <SPI.h>
 #include <SD.h>
@@ -45,37 +48,36 @@
 
 #define SD_CS    16  // Chip select line for SD card
 
-#define ROTARY_BTN  15 //Button of rotatry encoder
-#define ROTARY_A  14 //Pulse A of rotary encoder;
-#define ROTARY_B  4 //Pulse A of rotary encoder;
+#define JOY_BTN  4 //Button of joy stick
+#define JOY_Y  14 //Y-pos of joy stick
+#define JOY_X  15 //X-pos of joy stick
 
-#define MAIN_MENU 0
-#define SUB_MENU 1
-#define BROWSER 10
-#define TEXTFILE 11
+//some constants
+#define NOSELECT 0
+#define MAIN_MENU 1
+#define SUB_MENU 2
+#define BROWSER 3
+#define TEXTFILE 4
 
+//examples of menu definition
 String main_menu[] = {"Submenu","Codeset","SD-Card","Entry4","Entry5","Entry6","Entry7","Entry8","Entry9","Entry10","Entry11","Entry12","Entry13","Entry14","Entry15","Back"};
 uint8_t main_menu_cnt = 16;
 
 String sub_menu[] = {"Sub1","Sub2","Sub3","Back"};
 uint8_t sub_menu_cnt = 4;
 
-volatile int pos = 0;
-volatile int whichMenu = 0;
-String lastPath = "";
+volatile int tmp = 0; //to read joy values;
+volatile int whichMenu = 0; //define menu to switch handler
+String lastPath = ""; //to allow .. command in browser
 
 
 
 //tft instance
 Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
-RotaryEncoder encoder(ROTARY_A, ROTARY_B);
+//HC sreen library
 HCScreen screen = HCScreen(tft);
 
-//interrupt for rotary pulse
-void IRAM_ATTR rotaryPulse() {
-  encoder.tick();
-}
-
+//a select event for main menu occured
 void handleMainMenu(String selection, int8_t selectionIndex) {
   if (selection == "Back") {
   } else {
@@ -85,7 +87,7 @@ void handleMainMenu(String selection, int8_t selectionIndex) {
         whichMenu = SUB_MENU;
         break;
       case 1: screen.showCodeset();
-        whichMenu = TEXTFILE;
+        whichMenu = NOSELECT;
         break;
       case 2: screen.setDirectory("/DCIM",SD_CS);
         whichMenu = BROWSER;
@@ -94,6 +96,7 @@ void handleMainMenu(String selection, int8_t selectionIndex) {
   }
 }
 
+//a select event for sub menu occured
 void handleSubMenu(String selection, int8_t selectionIndex) {
   if (selection == "Back") {
       screen.setMenu(main_menu,main_menu_cnt);
@@ -105,15 +108,19 @@ void handleSubMenu(String selection, int8_t selectionIndex) {
   }
 }
 
+//a select event for browser occured
 void handleBrowser(String selection, int8_t selectionIndex) {
-  if (selection == "..") {
+
+  if (selection == "..") { //the go back entry was selected
     String path = screen.getTitle();
     Serial.println(path);
     if ((path == "Error") || (path == "/")) {
+      //if error or root menu go back to parent menu
       screen.setMenu(main_menu,main_menu_cnt);
       screen.setTitle("Main Menu");
       whichMenu=MAIN_MENU;
     } else {
+      //otherwise set path one step up
       uint8_t idx = path.lastIndexOf("/");
       if (idx == 0) {
         path = "/";
@@ -123,8 +130,11 @@ void handleBrowser(String selection, int8_t selectionIndex) {
       screen.setDirectory(path,SD_CS);
     }
   } else {
-    if (selection.startsWith("*")) screen.setDirectory(selection.substring(1),SD_CS);
-    if (selection.endsWith(".txt") || selection.endsWith(".TXT")) {
+    //selected entry starts with an asteriks means it is a directory
+    if (selection.startsWith("*")) {
+      screen.setDirectory(selection.substring(1),SD_CS);
+    } else if (selection.endsWith(".txt") || selection.endsWith(".TXT")) {
+      //if it is a text file we will display the content
       lastPath = screen.getTitle();
       screen.setTextFile(screen.getTitle(),selection);
       Serial.println(lastPath);
@@ -135,8 +145,8 @@ void handleBrowser(String selection, int8_t selectionIndex) {
 
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
+  //important initialize the display
   tft.initR(INITR_BLACKTAB);
   tft.setRotation(3);
   tft.fillScreen(ST7735_BLACK);
@@ -146,32 +156,35 @@ void setup() {
   screen.setTitle("Main Menu");
   screen.setLineHeight(10);
   //define input pins
-  pinMode(ROTARY_BTN, INPUT_PULLUP);
-  pinMode(ROTARY_A, INPUT_PULLUP);
-  pinMode(ROTARY_B, INPUT_PULLUP);
-  //define interrupts
-  digitalWrite(SD_CS,0);
-  delay(100);
-  digitalWrite(SD_CS,1);
-  attachInterrupt(digitalPinToInterrupt(ROTARY_A),rotaryPulse, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(ROTARY_B),rotaryPulse, CHANGE);
+  pinMode(JOY_BTN, INPUT_PULLUP);
+  pinMode(JOY_Y, INPUT_PULLUP);
+  pinMode(JOY_X, INPUT_PULLUP);
+  while (digitalRead(JOY_BTN)==0) delay(100);
 
 }
 
 void loop() {
-  int newPos = encoder.getPosition();
-  if (pos != newPos) {
-    if (newPos < 0) {
+  
+    //read joystick X currently not used
+    tmp = analogRead(JOY_X);
+    //read joystick y value is between 0 and 4096
+    tmp = analogRead(JOY_Y);
+    if (tmp > 3000) {
+      //if value > 3000 we select the next entry of a menu
+      //or scroll down if no selection active
       screen.selectNext();
-    } else {
+    };
+    if (tmp < 1000) {
+      //if value < 1000 we select the previous entry of a menu
+      //or scroll up if no selection active
       screen.selectPrevious();
     }
-    encoder.setPosition(0);
-    pos=0;
-  }
-
-    if (digitalRead(ROTARY_BTN)==0) {
-    while (digitalRead(ROTARY_BTN) == 0) delay(100);
+    
+  //select button of the joystick
+  if (digitalRead(JOY_BTN)==0) {
+    //wait until button was released
+    while (digitalRead(JOY_BTN) == 0) delay(10);
+    //we get current selection from the library
     String selection = screen.getSelection();
     int8_t selectionIndex = screen.getSelectionIndex();
     switch (whichMenu) {
@@ -184,8 +197,10 @@ void loop() {
       case TEXTFILE: screen.setDirectory(lastPath,SD_CS);
         whichMenu = BROWSER;
         break;
+      default : screen.setMenu(main_menu,main_menu_cnt);
+        screen.setTitle("Main menu");
+        whichMenu = MAIN_MENU;
+        break;
     }
   }
-
-
 }
